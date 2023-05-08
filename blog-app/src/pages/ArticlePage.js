56007966c16bf4da4ce6cd443d4c17cbc074b627
egreen19 <85269198+egreen19@users.noreply.github.com>
2023-05-08 10:33:33 -0400

    added login and signup functionality to frontend

--- blog-app/src/pages/ArticlePage.js
+++ blog-app/src/pages/ArticlePage.js
@@ -4,10 +4,13 @@ import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ votes: 0, comments: [] });
   const { articleId } = useParams();
+
+  const {user, isloading} = useUser();
 
   useEffect(() => {
     const loadArticle = async () => {
@@ -35,13 +38,19 @@ const ArticlePage = () => {
     <>
       <h1>{article.title}</h1>
       <div className="upvotes-section">
-        <button onClick={upvote}>Upvote</button>
+      {user 
+        ? <button onClick={upvote}>Upvote</button>
+        : <button>Login to upvote</button>
+      }
         <p>This article has {articleInfo.votes} upvote(s)</p>
       </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
-      <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+      {user 
+        ? <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        : <button>Login to comment</button>
+      }
       <CommentsList comments={articleInfo.comments} />
     </>
   )
--- blog-app/src/pages/CreateAccountPage.js
+++ blog-app/src/pages/CreateAccountPage.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const CreateAccountPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const createAccount = async () => {
+    try {
+      if(password!== confirmPassword) {
+        setError('Passwords do not match');
+        return;
+      }
+      await createUserWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
-    <div>CreateAccountPage</div>
+    <>
+      <h1>Create Account</h1>
+      {error && <p className='error'>{error}</p>}
+      <input placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+      <input type="password" placeholder='Re-Enter Password' value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+      <button onClick={createAccount}>Create Account</button>
+      <Link to="/login">Already have an account? Login!</Link>
+    </>
   )
 }
 
